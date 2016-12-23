7c04cbf356ffed56da4cf19e42657d46147dbfc9
dekapx <dekapx@gmail.com>
2016-12-23 14:58:31 +0000

    scala version upgrade

--- scala-quickstart/src/main/scala/com/kapx/scala/basics/MapDemo.scala
+++ scala-quickstart/src/main/scala/com/kapx/scala/basics/MapDemo.scala
@@ -3,5 +3,6 @@ package com.kapx.scala.basics
 object MapDemo extends App {
     var capital = Map("US" -> "Washington", "France" -> "Paris")
     capital += ("Japan" -> "Tokyo")
-    println(capital("France"))
+    capital += ("Ireland" -> "Dublin")
+    println(capital("Ireland"))
 }
