70b7e5132ed887135f670b8ee7eab9a4df981287
dekapx <dekapx@gmail.com>
2017-01-14 22:44:25 +0000

    scala variables

---
+++ scala-quickstart/src/main/scala/com/kapx/scala/basics/VariableDemo.scala
@@ -0,0 +1,18 @@
+package com.kapx.scala.basics
+
+object VariableDemo {
+    val message : String = "This is showcasing scala...";
+
+    def main(args: Array[String]) {
+        println(message);
+        
+        val num = 10;
+        val str = "Hello Scala";
+        println(num);
+        println(str);
+
+        val (name: String, marks: Int) = Pair("Dave", 25)
+        println(name);
+        println(marks)
+    }
+}
